50973eb9c6cf1a472cd1e0f64576db684048f057
zakariaaithssain <zakariaithssain@gmail.com>
2026-01-30 22:23:51 +0100

    write script to launch the training

--- train.py
+++ train.py
@@ -2,22 +2,10 @@ import torch
 import joblib
 import logging
 
+from mlp import MultiLayerPerceptron
 from config import READY_DATA_PATH
 
 
-
-logger = logging.Logger("train")
-data = joblib.load(READY_DATA_PATH)
-
-training_data = data["train"]
-X = training_data["X"]
-y = training_data["y"]
-#convert to tensors
-X = torch.tensor(X)
-y = torch.tensor(y)
-#the number of features has increased because of One Hot encoding. 
-print(X.shape)
-print(y.shape)
 
 
 
@@ -57,3 +45,34 @@ def train(model, dataloader, num_epochs:int, lr:float, device) -> list[dict]:
         
                         
 
+
+if __name__ == "__main__": 
+    logger = logging.Logger("train")
+    data = joblib.load(READY_DATA_PATH)
+
+    training_data = data["train"]
+    X = training_data["X"]
+    y = training_data["y"]
+    #convert to tensors
+    X = torch.tensor(X, dtype=torch.float32) #float is required for gradient calcs
+    y = torch.tensor(y, dtype=torch.long) #long type is issential for cross entropy
+
+    num_features = X.size(dim=1)
+    model = MultiLayerPerceptron(input_dim=num_features)
+    dataset = torch.utils.data.TensorDataset(X, y)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=37, shuffle=True,
+                                              pin_memory=True) #in case sehel 3lina Allah bshy GPU
+    num_epochs = 100
+    lr = 1e-3
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    try:
+        train(model, dataloader, num_epochs, lr, device)
+        exit(0)
+    except KeyboardInterrupt: 
+          logger.warning("training interrupted manually.")
+          exit(1)
+    
+    except Exception as e: 
+          logging.error(str(e))
+          exit(1)
